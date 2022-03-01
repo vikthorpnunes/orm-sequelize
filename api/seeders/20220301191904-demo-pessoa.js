d0e68fdffb4c8f302d6673ffa8f9c6ca2edc0e04
victor.nunes-at-931670397156 <victor.nunes@seidor.com.br>
2022-03-01 17:01:52 -0300

    refactor: demo pessoas added more seeds

--- api/seeders/20220301191904-demo-pessoa.js
+++ api/seeders/20220301191904-demo-pessoa.js
@@ -6,12 +6,52 @@ module.exports = {
       "Pessoas",
       [
         {
-          nome: 'Paula Morai',
+          nome: 'Ana Souza',
           ativo: true,
-          email: 'paula@test.com.br',
+          email: 'ana@ana.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Marcos Cintra',
+          ativo: true,
+          email: 'marcos@marcos.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Felipe Cardoso',
+          ativo: true,
+          email: 'felipe@felipe.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Sandra Gomes',
+          ativo: false,
+          email: 'sandra@sandra.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Paula Morais',
+          ativo: true,
+          email: 'paula@paula.com',
           role: 'docente',
           createdAt: new Date(),
-          updatedAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Sergio Lopes',
+          ativo: true,
+          email: 'sergio@sergio.com',
+          role: 'docente',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
