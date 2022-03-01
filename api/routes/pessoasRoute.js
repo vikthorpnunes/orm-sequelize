7dee2184dd6d4e7e5ef47aae00fad32f7962f8ff
victor.nunes-at-931670397156 <victor.nunes@seidor.com.br>
2022-03-01 17:41:20 -0300

    feat: added new route to get one person

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoa/:id', PessoaController.pegaUmaPessoa)
 
 module.exports = router
