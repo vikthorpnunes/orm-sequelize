8a72f21d47aab46cac4db1df5108d07323204d9f
victor.nunes-at-931670397156 <victor.nunes@seidor.com.br>
2022-03-01 17:43:14 -0300

    fix: wrong route for one person

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,6 +4,6 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
-router.get('/pessoa/:id', PessoaController.pegaUmaPessoa)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 
 module.exports = router
