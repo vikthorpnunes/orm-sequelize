ee8d1eb900c4742c6e73beb8a639ce11d44859e9
victor.nunes-at-931670397156 <victor.nunes@seidor.com.br>
2022-03-01 17:30:38 -0300

    refactor: added middlewear pessoas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser')
+const pessoas = require('./pessoasRoute')
 
 module.exports = app => {
     app.use(bodyParser.json())
+    app.use(pessoas)
     app.get('/', (req,res) => res.send('Ola!'))
 }
